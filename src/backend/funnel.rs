7560c502527d3f373bc641e9c504d6f4d3d6b561
joshradin <jradin16@gmail.com>
2025-05-10 15:31:06 -0700

    made reusable generic

--- src/backend/funnel.rs
+++ src/backend/funnel.rs
@@ -1,20 +1,23 @@
 use crate::backend::task::Data;
-use crate::promise::{BoxPromise, IntoPromise, PollPromise, Promise, PromiseSet};
+use crate::promise::{IntoPromise, PollPromise, Promise, PromiseSet};
 
 /// The backend funnel implementation.
 ///
 /// Funnels accepts inputs from tasks, and produces one output
 pub struct BackendFunnel {
-    promises: PromiseSet<'static, Data>
-}
-
-enum BackendFunnelInternal {
-    
+    promises: PromiseSet<'static, Data>,
 }
 
 impl BackendFunnel {
+    /// Creates a new backend funnel
+    pub fn new() -> Self {
+        Self {
+            promises: PromiseSet::new(),
+        }
+    }
+
     /// Inserts a promise into this backend funnel
-    pub fn insert<P: IntoPromise<Output=Data> + 'static>(&mut self, p: P) {
+    pub fn insert<P: IntoPromise<Output = Data> + 'static>(&mut self, p: P) {
         self.promises.insert(p.into_promise());
     }
 }
@@ -24,21 +27,19 @@ impl IntoPromise for BackendFunnel {
     type IntoPromise = BackendFunnelPromise;
 
     fn into_promise(self) -> Self::IntoPromise {
-        BackendFunnelPromise {
-            set: self.promises,
-        }
+        BackendFunnelPromise { set: self.promises }
     }
 }
 
 /// Backend future promise
 pub struct BackendFunnelPromise {
-    set: PromiseSet<'static, Data>
+    set: PromiseSet<'static, Data>,
 }
 
 impl Promise for BackendFunnelPromise {
     type Output = Vec<Data>;
 
     fn poll(&mut self) -> PollPromise<Self::Output> {
-        todo!()
+        self.set.poll()
     }
 }
--- src/backend/reusable.rs
+++ src/backend/reusable.rs
@@ -4,54 +4,113 @@ use crate::promise::{BoxPromise, GetPromise, PollPromise, Promise};
 use parking_lot::Mutex;
 use std::any::{type_name, Any};
 use std::fmt::{Debug, Formatter};
+use std::marker::PhantomData;
 use std::sync::Arc;
 
-#[derive(Clone)]
-pub struct Reusable {
-    state: Arc<Mutex<ReusableState>>,
-    cloner: Arc<dyn Fn(&Data) -> Data + Send + Sync>,
+pub struct Reusable<'lf, T, P = BoxPromise<'lf, T>>
+where
+    T: Send + Sync + 'lf,
+    P: Promise<Output = T> + 'lf,
+{
+    state: Arc<Mutex<ReusableState<'lf, T, P>>>,
+    cloner: Arc<dyn Fn(&T) -> T + Send + Sync + 'lf>,
 }
 
-impl Debug for Reusable {
+impl<'lf, T, P> Clone for Reusable<'lf, T, P>
+where
+    T: Send + Sync + 'lf,
+    P: Promise<Output = T> + 'lf,
+{
+    fn clone(&self) -> Self {
+        Self {
+            state: self.state.clone(),
+            cloner: self.cloner.clone(),
+        }
+    }
+}
+
+impl<'lf, T, P> Debug for Reusable<'lf, T, P>
+where
+    T: Send + Sync + 'lf,
+    P: Promise<Output = T> + 'lf,
+{
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Reusable").finish_non_exhaustive()
     }
 }
 
-enum ReusableState {
-    Init(BoxPromise<'static, Data>),
-    Data(Data),
+enum ReusableState<'lf, T: 'lf, P: Promise<Output = T> + 'lf>
+where
+    T: Send + Sync + 'lf,
+    P: Promise<Output = T> + 'lf,
+{
+    Init(P, PhantomData<&'lf T>),
+    Data(T),
 }
 
-impl Reusable {
-    pub fn new<P: Promise + 'static>(promise: P) -> Self
+impl<'lf, T: 'lf, P: Promise<Output = T> + 'lf> Reusable<'lf, T, P>
+where
+    T: Send + Sync + 'lf,
+    P: Promise<Output = T> + 'lf,
+{
+    pub fn new(promise: P) -> Self
     where
-        P::Output: Clone + Send + 'static,
+        T: Clone,
     {
-        let first_promise =
-            Box::new(promise.map(|promise| Box::new(promise) as Data)) as BoxPromise<Data>;
         let cloner = {
-            let closure = move |data: &Data| -> Data {
-                let downcast = data.downcast_ref::<P::Output>().unwrap_or_else(|| {
-                    panic!("Failed to downcast data to {}", type_name::<P::Output>())
-                });
-
-                let cloned = downcast.clone();
-                Box::new(cloned) as Data
-            };
-            Arc::from(Box::new(closure) as Box<dyn Fn(&Data) -> Data + Send + Sync>)
+            let closure = move |data: &T| -> T { data.clone() };
+            Arc::from(Box::new(closure) as Box<dyn Fn(&T) -> T + Send + Sync + 'lf>)
         };
 
         Reusable {
-            state: Arc::new(Mutex::new(ReusableState::Init(first_promise))),
+            state: Arc::new(Mutex::new(ReusableState::Init(promise, PhantomData))),
             cloner,
+        }
+    }
+
+    /// Converts this into a data promise. Panics if it can't take exclusive access over the interior.
+    pub fn to_data(self) -> Reusable<'static, Data, BoxPromise<'static, Data>>
+    where
+        T: Clone + 'static,
+        P: 'static,
+    {
+        let cloner = move |data: &Data| -> Data {
+            let downcast = data
+                .downcast_ref::<T>()
+                .unwrap_or_else(|| panic!("Failed to downcast data to {}", type_name::<T>()));
+
+            let cloned = downcast.clone();
+            Box::new(cloned) as Data
+        };
+        let state_mutex =
+            Arc::into_inner(self.state).expect("can not get exlusive access to interior");
+        let state = Mutex::into_inner(state_mutex);
+        match state {
+            ReusableState::Init(promise, _) => {
+                let first_promise =
+                    Box::new(promise.map(|promise| Box::new(promise) as Data)) as BoxPromise<Data>;
+                Reusable {
+                    state: Arc::new(Mutex::new(ReusableState::Init(first_promise, PhantomData))),
+                    cloner: Arc::new(cloner),
+                }
+            }
+            ReusableState::Data(data) => Reusable {
+                state: Arc::new(Mutex::new(ReusableState::Data(
+                    Box::new(data.clone()) as Data
+                ))),
+                cloner: Arc::new(cloner),
+            },
         }
     }
 }
 
-impl crate::promise::IntoPromise for Reusable {
-    type Output = Box<dyn Any + Send>;
-    type IntoPromise = IntoPromise;
+impl<'lf, T: Send + Sync + 'lf, P: Promise<Output = T> + 'lf> crate::promise::IntoPromise for Reusable<'lf, T, P>
+where
+    T: Send + Sync + 'lf,
+    P: Promise<Output = T> + 'lf,
+{
+    type Output = T;
+    type IntoPromise = IntoPromise<'lf, T, P>;
 
     fn into_promise(self) -> Self::IntoPromise {
         IntoPromise { inner: self }
@@ -59,18 +118,20 @@ impl crate::promise::IntoPromise for Reusable {
 }
 
 /// The reusable promise
-pub struct IntoPromise {
-    inner: Reusable,
+pub struct IntoPromise<'lf, T, P: Promise<Output = T> + 'lf>
+where T: Send + Sync + 'lf,
+{
+    inner: Reusable<'lf, T, P>,
 }
 
-impl Promise for IntoPromise {
-    type Output = Box<dyn Any + Send>;
+impl<'lf, T: Send + Sync + 'lf, P: Promise<Output = T> + 'lf> Promise for IntoPromise<'lf, T, P> {
+    type Output = T;
 
     fn poll(&mut self) -> PollPromise<Self::Output> {
         let Reusable { state, cloner } = &mut self.inner;
         let mut state = state.lock();
         match &mut *state {
-            ReusableState::Init(promise) => match promise.poll() {
+            ReusableState::Init(promise, _) => match promise.poll() {
                 PollPromise::Ready(ready) => {
                     let cloned = cloner(&ready);
                     *state = ReusableState::Data(ready);
@@ -94,8 +155,8 @@ mod tests {
     #[test]
     fn test_reusuable() {
         let reusable = Reusable::new(Just::new(123_i32));
-        let a = *reusable.clone().get().downcast::<i32>().unwrap();
-        let b = *reusable.get().downcast::<i32>().unwrap();
+        let a = reusable.clone().get();
+        let b = reusable.get();
         assert_eq!(a, b);
     }
 }
